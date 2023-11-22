07963f9046f05310f034fd216195e0cc2a323e75
Melaku Garcia <56188626+megarbon@users.noreply.github.com>
2023-11-22 23:57:26 +0100

    UI de la app de websockets completa
    falta terminar server

---
+++ web-sockets-app/server/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const port = process.env.PORT ?? 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/client/index.html");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
